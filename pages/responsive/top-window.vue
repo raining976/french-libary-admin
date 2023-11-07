94229e50a576eeaab31990b240a06bcb634d755a
raining <running.zhang@foxmail.com>
2023-11-07 23:50:41 +0800

    feat:添加顶部窗口

---
+++ pages/responsive/top-window.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="top-window">
+		法语图书后台管理系统
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	.top-window{
+		width: 100vw;
+		height: 100% ;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: #f8f8f8;
+	}
+</style>
